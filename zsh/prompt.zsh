b57d020f6bce0797d9b7857ce278212567f126ac
Jamie Connolly <jamie@jamieconnolly.com>
2013-10-30 11:42:08 +0000

    Update the ZSH prompt with some new niceness

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -22,49 +22,80 @@ git_dirty() {
   else
     if [[ "$st" =~ ^nothing ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "on %{$fg_bold[green]%}$(git_symbolic_ref)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo "on %{$fg_bold[red]%}$(git_symbolic_ref)%{$reset_color%}"
     fi
   fi
 }
 
-git_prompt_info () {
+git_prompt() {
+  echo "$(git_dirty)$(need_push)"
+}
+
+git_symbolic_ref() {
   ref=$($git symbolic-ref HEAD 2>/dev/null) || return
   echo "${ref#refs/heads/}"
+}
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo ""
+  else
+    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  fi
 }
 
 unpushed () {
   $git cherry -v origin/$(git_branch) 2>/dev/null
 }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
+python_prompt() {
+  if ! [[ -z $(python_version) ]]
   then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-virtualenv_info() {
-  [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`')'
-}
-
-virtualenv_prompt() {
-  if ! [[ -z "$(virtualenv_info)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(virtualenv_info)%{$reset_color%} "
+    echo "%{$fg_bold[yellow]%}$(python_version)%{$reset_color%} "
   else
     echo ""
   fi
 }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+python_version() {
+  if (( $+commands[pyenv] ))
+  then
+    version=$(pyenv version-name)
+    [ "$(pyenv global)" != "$version" ] && echo "python-$version"
+  fi
 }
 
-export PROMPT=$'\n$(virtualenv_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+virtualenv_info() {
+  [ $VIRTUAL_ENV ] && echo ''`basename $VIRTUAL_ENV`''
+}
+
+virtualenv_prompt() {
+  if ! [[ -z "$(virtualenv_info)" ]]
+  then
+    echo "%{$fg_bold[black]%}$(virtualenv_info)%{$reset_color%} "
+  else
+    echo ""
+  fi
+}
+
+current_pwd() {
+  current=$(pwd | sed -e "s,^$HOME,~,")
+  if [[ $current == "/" || $current == "~" ]]
+  then
+    echo $current
+  else
+    echo "$(basename $current)/"
+  fi
+}
+
+directory_name() {
+  echo "%{$fg_bold[cyan]%}$(current_pwd)%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(python_prompt)in $(directory_name) $(git_prompt)\n$(virtualenv_prompt)» '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
