c38e9ff11d0c074494471dd5b2b208b6d8c9c0dc
Jamie Connolly <jamie@jamieconnolly.com>
2014-06-30 11:56:45 +0100

    Add in a few helpful aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,12 @@
-alias reload!='. ~/.zshrc'
+alias cl="clear"
+alias count="wc -l"
+alias e="$EDITOR"
+alias l="ls -la"
+alias reload!=". ~/.zshrc"
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
