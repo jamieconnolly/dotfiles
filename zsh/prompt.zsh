bbb9b76939c5a20b58a43b1a89e4f855f7d1d585
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2014-09-11 21:16:10 +0100

    Add back in virtualenv info to the zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,71 +1,62 @@
 autoload colors && colors
 
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_symbolic_ref)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_symbolic_ref)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt() {
-  echo "$(git_dirty)$(need_push)"
-}
-
-git_symbolic_ref() {
-  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-  echo "${ref#refs/heads/}"
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo ""
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
 current_pwd() {
   current=$(pwd | sed -e "s,^$HOME,~,")
-  if [[ $current == "/" || $current == "~" ]]
-  then
+  if [[ $current == "/" || $current == "~" ]]; then
     echo $current
   else
     echo "$(basename $current)/"
   fi
 }
 
-directory_name() {
+dirname_prompt() {
   echo "%{$fg_bold[cyan]%}$(current_pwd)%{$reset_color%}"
 }
 
-export PROMPT=$'\nin $(directory_name) $(git_prompt)\n» '
-set_prompt () {
+git_branch() {
+  if (( $+commands[git] )); then
+    echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  fi
+}
+
+git_dirty() {
+  if (( $+commands[git] )); then
+    echo $(git status --porcelain)
+  fi
+}
+
+git_unpushed() {
+  if (( $+commands[git] )); then
+    echo $(git cherry -v @{upstream} 2>/dev/null)
+  fi
+}
+
+git_prompt() {
+  if (( $+commands[git] )) && ($(git rev-parse --git-dir >/dev/null 2>&1)); then
+    if [[ $(git_dirty) == "" ]]; then
+      echo -n "on %{$fg_bold[green]%}$(git_branch)%{$reset_color%} "
+    else
+      echo -n "on %{$fg_bold[red]%}$(git_branch)%{$reset_color%} "
+    fi
+
+    if [[ $(git_unpushed) != "" ]]; then
+      echo "with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    fi
+  fi
+}
+
+virtualenv_info() {
+  [ $VIRTUAL_ENV ] && echo ''`basename $VIRTUAL_ENV`''
+}
+
+venv_prompt() {
+  if ! [[ -z "$(virtualenv_info)" ]]; then
+    echo "%{$fg_bold[yellow]%}$(virtualenv_info)%{$reset_color%} "
+  fi
+}
+
+export PROMPT=$'\n$(venv_prompt)in $(dirname_prompt) $(git_prompt)\n» '
+set_prompt() {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
 
