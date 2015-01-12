f487625a3374c61a8b8648c22e8f7c1bedad017b
Jamie Connolly <jamie@jamieconnolly.com>
2015-01-12 16:48:07 +0000

    Add an alias to generate a cryptographically secure secret key

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,3 +12,6 @@ alias flush="dscacheutil -flushcache"
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
+
+# Generate a cryptographically secure secret key
+alias secret="ruby -e \"require 'securerandom'; puts SecureRandom.hex(64)\""
