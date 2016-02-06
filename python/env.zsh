48754e0b0f0be743e0b240ad07ce69284a2cc918
Jamie Connolly <jamie@jamieconnolly.com>
2016-02-06 15:29:09 +0000

    Add in PYENV_VIRTUALENV_FILE and an alias for script/update

--- python/env.zsh
+++ python/env.zsh
@@ -1,1 +1,2 @@
+export PYENV_VIRTUALENV_FILE=.python-venv
 export PYTHONSTARTUP=~/.pythonrc
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,3 +1,4 @@
 alias sb="script/bootstrap"
 alias ss="script/server"
 alias st="script/test"
+alias st="script/update"
