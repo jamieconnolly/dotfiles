1e10573dc82f8beac037d16ec03ebd952f01888f
Jamie Connolly <jamie@jamieconnolly.com>
2016-03-23 22:08:40 +0000

    Update the _pyenv_virtualenv_hook function

--- python/virtualenv.zsh
+++ python/virtualenv.zsh
@@ -1,33 +1,24 @@
 function _pyenv_find_local_venv_file() {
   local root="$1"
-  while true; do
-    [[ "$root" =~ ^//[^/]*$ ]] && break
+  while ! [[ "$root" =~ ^//[^/]*$ ]]; do
     if [ -e "${root}/.python-venv" ]; then
       echo "${root}/.python-venv"
-      exit
-    elif [ -e "${root}/.pyenv-venv" ]; then
-      echo "${root}/.pyenv-venv"
-      exit
+      return 0
     fi
     [ -n "$root" ] || break
     root="${root%/*}"
   done
+  return 1
 }
 
 function _pyenv_virtualenv_hook() {
   local ret=$?
-
   local virtualenv="$(pyenv version-file-read $(_pyenv_find_local_venv_file $PWD) || true)"
-  local prefix="$(pyenv prefix $virtualenv 2>/dev/null || true)"
 
   if [ -n "$VIRTUAL_ENV" ]; then
-    if [ -n "$virtualenv" ]; then
-      eval "$(pyenv sh-activate --quiet $virtualenv || true)" || true
-    else
-      eval "$(pyenv sh-deactivate --quiet || true)" || true
-    fi
+    eval "$(pyenv sh-activate --quiet $virtualenv 2>/dev/null || pyenv sh-deactivate --quiet || true)" || true
   else
-    eval "$(pyenv sh-activate --quiet $virtualenv || true)" || true
+    eval "$(pyenv sh-activate --quiet $virtualenv 2>/dev/null || true)" || true
   fi
   return $ret
 }
