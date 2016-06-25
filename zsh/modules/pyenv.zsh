29253b1e3bdbe6fb3635e7a02ba4372705a82aee
Jamie Connolly <jamie@jamieconnolly.com>
2016-06-25 01:07:05 +0100

    Simplify the pyenv and rbenv prompt versions

--- zsh/modules/pyenv.zsh
+++ zsh/modules/pyenv.zsh
@@ -2,14 +2,9 @@ if command -v pyenv >/dev/null 2>&1; then
   eval "$(pyenv init - --no-rehash)"
 
   function pyenv_prompt() {
-    local python_version python_virtualenv
-    python_version="$(pyenv virtualenv-version || pyenv version-name)"
-    python_virtualenv="$(pyenv virtualenv-name)"
+    local python_version="$(pyenv virtualenv-version || pyenv version-name)"
 
-    if [[ -n "$python_virtualenv" ]]; then
-      echo "$ZSH_THEME_PYENV_PROMPT_PREFIX$python_version ($python_virtualenv)$ZSH_THEME_PYENV_PROMPT_SUFFIX"
-      return 0
-    elif [[ -n "$python_version" ]] && [[ $python_version != "system" ]]; then
+    if [[ -n "$python_version" ]] && [[ $python_version != "system" ]]; then
       echo "$ZSH_THEME_PYENV_PROMPT_PREFIX$python_version$ZSH_THEME_PYENV_PROMPT_SUFFIX"
       return 0
     else
