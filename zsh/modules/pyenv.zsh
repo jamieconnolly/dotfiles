1c843b3bb100bb0301a3093eb2af6fdfeb567b99
Jamie Connolly <jamie@jamieconnolly.com>
2016-06-29 23:55:01 +0100

    Update the nodenv, pyenv and rbenv prompts

--- zsh/modules/pyenv.zsh
+++ zsh/modules/pyenv.zsh
@@ -2,10 +2,8 @@ if command -v pyenv >/dev/null 2>&1; then
   eval "$(pyenv init - --no-rehash)"
 
   function pyenv_prompt() {
-    local python_version="$(pyenv virtualenv-version || pyenv version-name)"
-
-    if [[ -n "$python_version" ]] && [[ $python_version != "system" ]]; then
-      echo "$ZSH_THEME_PYENV_PROMPT_PREFIX$python_version$ZSH_THEME_PYENV_PROMPT_SUFFIX"
+    if [ -n "$(pyenv version-file $PWD)" ]; then
+      echo "$ZSH_THEME_PYENV_PROMPT_PREFIX$(pyenv version-name)$ZSH_THEME_PYENV_PROMPT_SUFFIX"
       return 0
     else
       return 1
