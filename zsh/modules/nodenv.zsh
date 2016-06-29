c541c186a53f23c5ccd5240cd917fb4fa7fbf0d8
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-06-29 23:55:01 +0100

    Update the nodenv, pyenv and rbenv prompts

--- zsh/modules/nodenv.zsh
+++ zsh/modules/nodenv.zsh
@@ -2,10 +2,8 @@ if command -v nodenv >/dev/null 2>&1; then
   eval "$(nodenv init - --no-rehash)"
 
   function nodenv_prompt() {
-    local node_version="$(nodenv version-name)"
-
-    if [[ -n "$node_version" ]] && [[ $node_version != "system" ]]; then
-      echo "$ZSH_THEME_NODENV_PROMPT_PREFIX$node_version$ZSH_THEME_NODENV_PROMPT_SUFFIX"
+    if [ -n "$(nodenv version-file $PWD)" ]; then
+      echo "$ZSH_THEME_NODENV_PROMPT_PREFIX$(nodenv version-name)$ZSH_THEME_NODENV_PROMPT_SUFFIX"
       return 0
     else
       return 1
--- zsh/modules/rbenv.zsh
+++ zsh/modules/rbenv.zsh
@@ -2,10 +2,8 @@ if command -v rbenv >/dev/null 2>&1; then
   eval "$(rbenv init - --no-rehash)"
 
   function rbenv_prompt() {
-    local ruby_version="$(rbenv version-name)"
-
-    if [[ -n "$ruby_version" ]] && [[ $ruby_version != "system" ]]; then
-      echo "$ZSH_THEME_RBENV_PROMPT_PREFIX$ruby_version$ZSH_THEME_RBENV_PROMPT_SUFFIX"
+    if [ -n "$(rbenv version-file $PWD)" ]; then
+      echo "$ZSH_THEME_RBENV_PROMPT_PREFIX$(rbenv version-name)$ZSH_THEME_RBENV_PROMPT_SUFFIX"
       return 0
     else
       return 1
