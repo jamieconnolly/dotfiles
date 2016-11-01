ac3ff68b6a6dab5402f652992d6108571759405f
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-11-01 17:21:05 +0000

    Explicitly tell nodenv, pyenv, and rbenv to use zsh

--- zsh/modules/hub.zsh
+++ zsh/modules/hub.zsh
@@ -1,3 +1,3 @@
 if command -v hub >/dev/null 2>&1; then
-  eval "$(hub alias -s)"
+  eval "$(hub alias -s zsh)"
 fi
--- zsh/modules/rbenv.zsh
+++ zsh/modules/rbenv.zsh
@@ -1,5 +1,5 @@
 if command -v rbenv >/dev/null 2>&1; then
-  eval "$(rbenv init - --no-rehash)"
+  eval "$(rbenv init --no-rehash - zsh)"
 fi
 
 function rbenv_prompt() {
