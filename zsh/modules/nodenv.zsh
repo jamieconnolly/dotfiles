a969f775b57fa89aab7f71c08ea9c8dab18584ba
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-04-18 18:13:49 +0100

    :fire: Remove version info from the prompt

--- zsh/modules/nodenv.zsh
+++ zsh/modules/nodenv.zsh
@@ -1,23 +1,3 @@
 if command -v nodenv >/dev/null 2>&1; then
   eval "$(nodenv init - zsh)"
 fi
-
-function nodenv_prompt() {
-  local nodenv_version nodenv_version_file
-  nodenv_version_file="$(nodenv version-file "$PWD" 2>/dev/null)"
-
-  if [[ -n "$nodenv_version_file" ]]; then
-    nodenv_version="$(nodenv version-file-read "$nodenv_version_file" || true)"
-
-    if [[ -n "$(nodenv prefix "${nodenv_version}" 2>/dev/null)" ]]; then
-      nodenv_prompt="${ZSH_THEME_VERSION_PROMPT_VALID_PREFIX}${nodenv_version}${ZSH_THEME_VERSION_PROMPT_VALID_SUFFIX}"
-    else
-      nodenv_prompt="${ZSH_THEME_VERSION_PROMPT_INVALID_PREFIX}${nodenv_version}${ZSH_THEME_VERSION_PROMPT_INVALID_SUFFIX}"
-    fi
-
-    echo "${ZSH_THEME_VERSION_PROMPT_PREFIX}${nodenv_prompt}${ZSH_THEME_VERSION_PROMPT_SUFFIX}"
-    return 0
-  else
-    return 1
-  fi
-}
--- zsh/modules/pyenv.zsh
+++ zsh/modules/pyenv.zsh
@@ -1,25 +1,3 @@
 if command -v pyenv >/dev/null 2>&1; then
   eval "$(pyenv init - zsh)"
 fi
-
-function pyenv_prompt() {
-  local pyenv_versions pyenv_version_file
-  pyenv_version_file="$(pyenv version-file "$PWD" 2>/dev/null)"
-
-  if [[ -n "$pyenv_version_file" ]]; then
-    OLDIFS="$IFS"
-    IFS=: pyenv_versions=($(pyenv version-file-read "$pyenv_version_file" || true))
-    IFS="$OLDIFS"
-
-    if [[ -n "$(pyenv prefix "${pyenv_versions[1]}" 2>/dev/null)" ]]; then
-      pyenv_prompt="${ZSH_THEME_VERSION_PROMPT_VALID_PREFIX}${pyenv_versions[1]}${ZSH_THEME_VERSION_PROMPT_VALID_SUFFIX}"
-    else
-      pyenv_prompt="${ZSH_THEME_VERSION_PROMPT_INVALID_PREFIX}${pyenv_versions[1]}${ZSH_THEME_VERSION_PROMPT_INVALID_SUFFIX}"
-    fi
-
-    echo "${ZSH_THEME_VERSION_PROMPT_PREFIX}${pyenv_prompt}${ZSH_THEME_VERSION_PROMPT_SUFFIX}"
-    return 0
-  else
-    return 1
-  fi
-}
