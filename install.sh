6f0f8b96f9325d4f9d17e1a9ffdf9de0d53d95b6
Jamie Connolly <jamie@jamieconnolly.com>
2022-08-15 18:12:34 +0100

    Disable the custom installation

--- install.sh
+++ install.sh
@@ -4,36 +4,36 @@
 
 set -e
 
-DOTFILES_ROOT="$(pwd -P)"
+# DOTFILES_ROOT="$(pwd -P)"
 
-for SRC_PATH in "$DOTFILES_ROOT"/*; do
-  [[ "$SRC_PATH" == **/Library ]] && continue
-  [[ "$SRC_PATH" == **/script ]] && continue
-  [[ "$SRC_PATH" == **/*.md ]] && continue
-  [[ "$SRC_PATH" == **/*.sh ]] && continue
+# for SRC_PATH in "$DOTFILES_ROOT"/*; do
+  # [[ "$SRC_PATH" == **/Library ]] && continue
+  # [[ "$SRC_PATH" == **/script ]] && continue
+  # [[ "$SRC_PATH" == **/*.md ]] && continue
+  # [[ "$SRC_PATH" == **/*.sh ]] && continue
 
-  DEST_PATH="${HOME}/.${SRC_PATH##${DOTFILES_ROOT}/}"
-  if [[ ! -e "$DEST_PATH" ]] || [[ "$(readlink "$DEST_PATH")" != "$SRC_PATH" ]]; then
-    if [[ -L "$DEST_PATH" ]] && [[ ! -d "$SRC_PATH" ]]; then
-      ln -sfv "$SRC_PATH" "$DEST_PATH"
-    else
-      rm -rfv "$DEST_PATH"
-      ln -sv "$SRC_PATH" "$DEST_PATH"
-    fi
-  fi
-done
+  # DEST_PATH="${HOME}/.${SRC_PATH##${DOTFILES_ROOT}/}"
+  # if [[ ! -e "$DEST_PATH" ]] || [[ "$(readlink "$DEST_PATH")" != "$SRC_PATH" ]]; then
+    # if [[ -L "$DEST_PATH" ]] && [[ ! -d "$SRC_PATH" ]]; then
+      # ln -sfv "$SRC_PATH" "$DEST_PATH"
+    # else
+      # rm -rfv "$DEST_PATH"
+      # ln -sv "$SRC_PATH" "$DEST_PATH"
+    # fi
+  # fi
+# done
 
-if [[ "$OSTYPE" == darwin* ]]; then
-  for SRC_PATH in "$DOTFILES_ROOT"/Library/**/*; do
-    DEST_PATH="${HOME}/${SRC_PATH##${DOTFILES_ROOT}/}"
-    if [[ ! -e "$DEST_PATH" ]] || [[ "$(readlink "$DEST_PATH")" != "$SRC_PATH" ]]; then
-      mkdir -p "${DEST_PATH%/*}"
-      if [[ -L "$DEST_PATH" ]] && [[ ! -d "$SRC_PATH" ]]; then
-        ln -sfv "$SRC_PATH" "$DEST_PATH"
-      else
-        rm -rfv "$DEST_PATH"
-        ln -sv "$SRC_PATH" "$DEST_PATH"
-      fi
-    fi
-  done
-fi
+# if [[ "$OSTYPE" == darwin* ]]; then
+  # for SRC_PATH in "$DOTFILES_ROOT"/Library/**/*; do
+    # DEST_PATH="${HOME}/${SRC_PATH##${DOTFILES_ROOT}/}"
+    # if [[ ! -e "$DEST_PATH" ]] || [[ "$(readlink "$DEST_PATH")" != "$SRC_PATH" ]]; then
+      # mkdir -p "${DEST_PATH%/*}"
+      # if [[ -L "$DEST_PATH" ]] && [[ ! -d "$SRC_PATH" ]]; then
+        # ln -sfv "$SRC_PATH" "$DEST_PATH"
+      # else
+        # rm -rfv "$DEST_PATH"
+        # ln -sv "$SRC_PATH" "$DEST_PATH"
+      # fi
+    # fi
+  # done
+# fi
