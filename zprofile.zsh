c3a3996ffa35d4919b31996bd5432414df3163ea
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2022-08-15 22:18:24 +0100

    Update list of installed packages and PATH location

--- zprofile.zsh
+++ zprofile.zsh
@@ -1,8 +1,15 @@
-typeset -gU cdpath
+typeset -gU cdpath path
 
 # Set the list of directories that cd searches
 cdpath=(
   ${CODE_HOME}/$(git config github.user)
   ${CODE_HOME}
   ${cdpath[@]}
+)
+
+# Set the list of directories that contain executables
+path=(
+  /usr/local/bin
+  /opt/homebrew/bin
+  ${path[@]}
 )
