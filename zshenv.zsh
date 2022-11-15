bb7520bbba4415f9c13d2abaef9facfe66f664fd
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2022-11-15 14:14:55 +0000

    Set GPR_TOKEN for Codespaces

--- zshenv.zsh
+++ zshenv.zsh
@@ -23,6 +23,8 @@ export LC_CTYPE=$LANG
 # Secrets
 if [ "$(uname -s)" = "Darwin" ]; then
   export GPR_TOKEN="op://Personal/GitHub/credentials/gpr_token"
+elif [ -n "$CODESPACES" ]; then
+  export GPR_TOKEN=$GITHUB_TOKEN
 fi
 
 # SSH
